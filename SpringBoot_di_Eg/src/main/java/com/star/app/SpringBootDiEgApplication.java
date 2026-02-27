cc4ffb3874be67148a89549eb003ac356949135c
Hardi <hardivirani2003@gmail.com>
2026-02-27 09:04:20 -0500

    Project Created

---
+++ SpringBoot_di_Eg/src/main/java/com/star/app/SpringBootDiEgApplication.java
@@ -0,0 +1,13 @@
+package com.star.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiEgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDiEgApplication.class, args);
+    }
+
+}
